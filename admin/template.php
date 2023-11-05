f5002a49d80d6387bb85056df7ea6b89e7d30b36
minnatur-rahman <minnatur7373@gmail.com>
2023-11-06 05:33:28 +0600

    some change data

---
+++ admin/template.php
@@ -0,0 +1,19 @@
+<?php include_once('includes/head.php') ?>
+
+<body class="sb-nav-fixed">
+    <?php include_once('includes/topnav.php') ?>
+    <div id="layoutSidenav">
+        <?php include_once('includes/sidenav.php') ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container">
+                    <H1>hi</H1>
+                </div>
+            </main>
+            <?php include_once('includes/footer.php') ?>
+        </div>
+    </div>
+    <?php include_once('includes/scripts.php') ?>
+</body>
+
+</html>
