3dcb7d9ae01fc192444a692e03694595165b6aab
minnatur-rahman <minnatur7373@gmail.com>
2023-11-10 00:31:50 +0600

    today last commit'

--- admin/view/manage_cat_view.php
+++ admin/view/manage_cat_view.php
@@ -1,1 +1,22 @@
 <h1>Manage category page</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Category Name</th>
+            <th>Category Description</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>some</td>
+            <td>Her name is shabina</td>
+            <td>
+                <a class="btn btn-warning" href="">Edit</a>
+                <a class="btn btn-danger" href="">Delete</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
