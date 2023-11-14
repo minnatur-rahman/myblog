33dc4ae7f3aa8cd592648796bd27f1c1e121c4c3
minnatur-rahman <minnatur7373@gmail.com>
2023-11-14 06:44:30 +0600

    today my last commit

--- index.php
+++ index.php
@@ -1,28 +1,38 @@
-<?php include_once('include/head.php') ?>
+<?php include('admin/class/function.php');
+
+$obj = new myAdminBlog();
+$getCat = $obj->display_category();
+
+
+?>
+
+
+
+<?php include_once('include/head.php'); ?>
 
 <body>
 
-    <?php include_once('include/preloader.php') ?>
+    <?php include_once('include/preloader.php'); ?>
 
-    <?php include_once('include/header.php') ?>
+    <?php include_once('include/header.php'); ?>
 
     <!-- Page Content -->
     <!-- Banner Starts Here -->
-    <?php include_once('include/banar.php') ?>
+    <?php include_once('include/banar.php'); ?>
     <!-- Banner Ends Here -->
 
-    <?php include_once('include/cta.php') ?>
+    <?php include_once('include/cta.php'); ?>
 
 
     <section class="blog-posts">
         <div class="container">
             <div class="row">
-                <?php include_once('<include/blogpost.php') ?>
-                <?php include_once('include/sidebar.php') ?>
+                <?php include_once('<include/blogpost.php'); ?>
+                <?php include_once('include/sidebar.php'); ?>
             </div>
         </div>
     </section>
 
 
-    <?php include_once('include/footer.php') ?>
-    <?php include_once('include/script.php') ?>
+    <?php include_once('include/footer.php'); ?>
+    <?php include_once('include/script.php'); ?>
