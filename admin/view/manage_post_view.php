cc7cb556ace5325e1520281f7dbd53f1e929fceb
minnatur-rahman <minnatur7373@gmail.com>
2023-11-17 09:02:35 +0600

    edit soome structure

--- admin/view/manage_post_view.php
+++ admin/view/manage_post_view.php
@@ -25,7 +25,7 @@ $posts = $obj->display_post();
                     <td><?php echo $postData['post_id']; ?></td>
                     <td><?php echo $postData['post_title']; ?></td>
                     <td><?php echo $postData['post_content']; ?></td>
-                    <td><img src="../upload/<?php echo $postData['post_img']; ?>" alt=""></td>
+                    <td><img height="100" src="../upload/<?php echo $postData['post_img']; ?>" alt=""></td>
                     <td><?php echo $postData['post_author']; ?></td>
                     <td><?php echo $postData['post_date']; ?></td>
                     <td><?php echo $postData['cat_name']; ?></td>
