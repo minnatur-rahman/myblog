387494933ceae033e302e9e916af86f0192b30e3
millatur <minnatur7373@gmail.com>
2023-11-21 06:52:32 +0600

    morning commit

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@
      <nav class="navbar navbar-expand-lg">
          <div class="container">
              <a class="navbar-brand" href="index.php">
-                 <h2>Stand Blog<em>.</em></h2>
+                 <h2>ADIYAT RAHMAN<em>.</em></h2>
              </a>
              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                  <span class="navbar-toggler-icon"></span>
